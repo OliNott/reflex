285e13de72b89cd7472bda50d8a2d9935c25d516
Olivier Nottebart <olivier.nottebart@gmail.com>
2019-12-31 18:44:23 +0100

    Create Element sperate children element from text elements, tests need to be written

--- test/createElement.test.js
+++ test/createElement.test.js
@@ -1,9 +1,41 @@
-const createElement = require('../lib/createElement')
+const Reflex = require('../lib/Reflex')
 
+// createElement("div")
 test('Create an empty element should have the right structure', () => {
   const result = {
     "type": "div",
     "props": { "children": [] }
   }
-  expect(createElement("div")).toMatchObject(result)
+
+  expect(Reflex.createElement("div")).toMatchObject(result)
 })
+
+// createElement("div", null, a)
+test('Create an element without inherited props and with one children should have the right structure', () => {
+  const result = {
+      "type": "div",
+      "props": { "children": [{ a: 'a' }] }
+    }
+
+  expect(Reflex.createElement("div", null, { a: 'a' })).toMatchObject(result)
+})
+
+// createElement("div", null, a, b)
+test('Create an element without inherited props and with one children should have the right structure', () => {
+  const result = {
+      "type": "div",
+      "props": { "children": [{ a: 'a' }, { b: 'b' }] }
+    }
+
+  expect(Reflex.createElement("div", null, { a: 'a' }, { b: 'b' })).toMatchObject(result)
+})
+
+// createElement("div", null, a, b)
+// test('Create an element a text element instead of text', () => {
+//   const result = {
+//       "type": "div",
+//       "props": { "children": [{ a: 'a' }, { b: 'b' }] }
+//     }
+
+//   expect(Reflex.createElement("div", null, 'a')).toMatchObject(result)
+// })
